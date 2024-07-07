b83df0e274cb6d16b94e00452d8dd756180f2032
Zahra <azhayat3@gmail.com>
2024-07-08 00:21:10 +0500

    changed-file-names

--- src/redux/slices/pet.slice.tsx
+++ src/redux/slices/pet.slice.tsx
@@ -1,8 +1,4 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-// import {
-//   addAuthorizationHeader,
-//   axiosInstance,
-// } from "../../constants/PetFinderApi";
 
 import { FetchLimitedPetsByTypesArgs, FetchPetsByTypesArgs, FetchSinglePetsByTypesArgs, PetState, PetsResponse, SinglePetResponse } from "../../@types/types";
 import { addAuthorizationHeader, axiosInstance } from "../../constants/PetFinderApi";
